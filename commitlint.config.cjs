41555a6c828de61335dec07e38cc9d508f90589d
Andrés Zorro <andres.zorro@formula.co>
2024-07-10 17:53:10 -0500

    ci(build): commitlint config fixes

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,6 +1,6 @@
 module.exports = {
     extends: ["@commitlint/config-conventional"],
-    ignores: [(commit) => commit.startsWith("[skip ci]")],
+    ignores: [(commit) => commit.startsWith("[skip-ci]")],
     rules: {
         "body-max-line-length": [0, "always", 200],
     },
