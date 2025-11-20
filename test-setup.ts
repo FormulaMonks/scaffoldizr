602e749a09344c72efe9b22923fdb282f3aa9a47
Andrés Zorro <andres.zorro@monks.com>
2025-11-20 12:17:32 -0500

    ci: updated bun and vscode settings

--- test-setup.ts
+++ test-setup.ts
@@ -5,6 +5,7 @@ import { $ } from "bun";
 afterAll(async () => {
     const results = await $`find . -name .test-generated`.text();
     for (const result of results.trim().split("\n")) {
+        if (!result) continue;
         await rm(result, { recursive: true });
     }
 });
